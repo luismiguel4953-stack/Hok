26dc7cde85c4f658763202144eb7a2338f42bfc9
CodeByLuis <luismiguel4953@gmail.com>
2026-08-11 16:56:02 +0100

    test: update app name expectation

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -11,11 +11,10 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [36])
 class ExampleRobolectricTest {
-
-  @Test
-  fun `read string from context`() {
-    val context = ApplicationProvider.getApplicationContext<Context>()
-    val appName = context.getString(R.string.app_name)
-    assertEquals("My Application", appName)
-  }
+    @Test
+    fun `read string from context`() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val appName = context.getString(R.string.app_name)
+        assertEquals("Game Turbo", appName)
+    }
 }
